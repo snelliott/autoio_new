00fba1c2a8884c6975df39b32412e06e4225d8dd
avcopan <avcopan@gmail.com>
2019-04-16 17:30:04 -0500

    Fixes bug in gaussian z-matrix parser

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 
 setup(name="elstruct",
-      version="0.2.7",
+      version="0.2.8",
       packages=["elstruct",
                 "elstruct.writer",
                 "elstruct.reader",
