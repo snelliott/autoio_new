65fcd2f7497b50cefbde5b1123aa399ac5325cfe
avcopan <avcopan@gmail.com>
2019-07-02 14:46:55 -0500

    Removes elcarro, which has been merged into moldriver

--- elcarro/__init__.py
+++
@@ -1,8 +0,0 @@
-""" elcarro: drivers for elstruct """
-from elcarro.drivers import feedback_optimization
-from elcarro.drivers import robust_run
-
-__all__ = [
-    'feedback_optimization',
-    'robust_run',
-]
