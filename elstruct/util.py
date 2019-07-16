ad52bd8cef72aa4d14da46a412b07559be549b64
avcopan <avcopan@gmail.com>
2019-07-16 15:37:53 -0500

    Adds trans-rot projection and fixes z-matrix optimization writer

--- elstruct/util.py
+++ elstruct/util.py
@@ -1,5 +1,6 @@
 """ front-end reader utilities
 """
+import functools
 import numpy
 from qcelemental import constants as qcc
 import automol
@@ -14,46 +15,73 @@ def rotational_constants(geo):
     return cons
 
 
-def normal_coordinates(geo, hess):
+def normal_coordinates(geo, hess, project=True):
     """ normal coordinates
     """
-    norm_coos, _, _ = _frequency_analysis(geo, hess)
+    norm_coos, _, _ = _frequency_analysis(geo, hess, project=project)
     return norm_coos
 
 
-def harmonic_frequencies(geo, hess):
+def harmonic_frequencies(geo, hess, project=True):
     """ harmonic frequencies (in cm^-1)
 
     (imaginary entries returned as negative)
     """
-    freqs = numpy.subtract(real_harmonic_frequencies(geo, hess),
-                           imaginary_harmonic_frequencies(geo, hess))
+    _, freqs_re, freqs_im = _frequency_analysis(geo, hess, project=project)
+    freqs = numpy.subtract(freqs_re, freqs_im)
     freqs = tuple(freqs)
     return freqs
 
 
-def real_harmonic_frequencies(geo, hess):
-    """ real harmonic frequencies
-    """
-    _, freqs_re, _ = _frequency_analysis(geo, hess)
-    return freqs_re
-
-
-def imaginary_harmonic_frequencies(geo, hess):
-    """ imaginary harmonic frequencies
-    """
-    _, _, freqs_im = _frequency_analysis(geo, hess)
-    return freqs_im
-
-
-def _frequency_analysis(geo, hess):
-    """ harmonic frequency analysis
+def mass_weighted_hessian(geo, hess, project=True):
+    """ translation/rotation-projected mass-weighted hessian
     """
     amas = automol.geom.masses(geo, amu=False)
     mw_vec = numpy.divide(1., numpy.sqrt(numpy.repeat(amas, 3)))
     mw_mat = numpy.outer(mw_vec, mw_vec)
     mw_hess = numpy.multiply(hess, mw_mat)
+    if project:
+        dim = len(mw_vec)
+        trans_norm_coos = translational_normal_coordinates(geo)
+        rot_norm_coos = rotational_normal_coordinates(geo)
+        tr_norm_coos = numpy.hstack([trans_norm_coos, rot_norm_coos])
+        tr_proj = numpy.eye(dim) - numpy.dot(tr_norm_coos, tr_norm_coos.T)
+        mw_hess = numpy.dot(numpy.dot(tr_proj, mw_hess), tr_proj)
+
     mw_hess = tuple(map(tuple, mw_hess))
+    return mw_hess
+
+
+def translational_normal_coordinates(geo, axes=None):
+    """ translational normal coordinates
+    """
+    if axes is None:
+        axes = numpy.eye(3)
+    mw_vec = _column_vector(numpy.sqrt(automol.geom.masses(geo)))
+    trans_norm_coos = _normalize_columns(numpy.kron(mw_vec, axes))
+    return trans_norm_coos
+
+
+def rotational_normal_coordinates(geo, axes=None):
+    """ rotational normal coordinates
+    """
+    if axes is None:
+        axes = numpy.eye(3)
+    xyzs = numpy.array(automol.geom.coordinates(geo))
+    mw_vec = _column_vector(numpy.sqrt(automol.geom.masses(geo)))
+    mw_xyzs = numpy.multiply(xyzs, mw_vec)
+    cross_mw_xyzs = functools.partial(numpy.cross, mw_xyzs)
+    rot_norm_coos = _normalize_columns(numpy.hstack(
+        list(map(_column_vector, map(cross_mw_xyzs, axes.T)))))
+    return rot_norm_coos
+
+
+def _frequency_analysis(geo, hess, project=True):
+    """ harmonic frequency analysis
+    """
+    amas = automol.geom.masses(geo, amu=False)
+    mw_vec = numpy.divide(1., numpy.sqrt(numpy.repeat(amas, 3)))
+    mw_hess = mass_weighted_hessian(geo, hess, project=project)
     fcs, mw_norm_coos = numpy.linalg.eigh(mw_hess)
 
     conv = qcc.conversion_factor("hartree", "wavenumber")
@@ -63,3 +91,22 @@ def _frequency_analysis(geo, hess):
 
     norm_coos = mw_vec * mw_norm_coos
     return norm_coos, freqs_re, freqs_im
+
+
+def _normalize_columns(mat):
+    """normalize the columns of a matrix
+    :param mat: matrix
+    :type mat: numpy.ndarray
+    :rtype: numpy.ndarray
+    """
+    norms = list(map(numpy.linalg.norm, numpy.transpose(mat)))
+    return numpy.divide(mat, norms)
+
+
+def _column_vector(vec):
+    """form an n x 1 column vector, flattening if necessary
+    :param vec: vector or array
+    :type vec: numpy.ndarray
+    :rtype: numpy.ndarray
+    """
+    return numpy.reshape(vec, (-1, 1))
