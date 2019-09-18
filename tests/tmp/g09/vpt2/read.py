eb44cf73c9c131f656d6f9a754e7f154afba1b53
Kevin Moore <kmoore@blueslogin4.lcrc.anl.gov>
2019-09-18 17:55:25 -0500

    update

--- tests/tmp/g09/vpt2/read.py
+++ tests/tmp/g09/vpt2/read.py
@@ -12,7 +12,7 @@ def test__vpt2_reader():
     with open('output.dat', 'r') as outfile:
         output_string = outfile.read()
 
-    vpt2_dict = elstruct.reader.vpt2('g09', output_string)
+    vpt2_dict = elstruct.reader.vpt2('gaussian09', output_string)
 
     for key, val in vpt2_dict.items():
         print('\n\n')
