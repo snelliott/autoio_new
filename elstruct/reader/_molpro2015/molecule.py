97b9b963edeffd886352af348a1dea7a0a15e313
Kevin Moore <kmoore@blueslogin1.lcrc.anl.gov>
2019-11-03 10:44:09 -0600

    fix reader for double hybrids for gaussian

--- elstruct/reader/_molpro2015/molecule.py
+++ elstruct/reader/_molpro2015/molecule.py
@@ -35,6 +35,22 @@ def opt_geometry(output_string):
     return geo
 
 
+def hess_geometry(output_string):
+    """ get the geometry associated with a hessian calculation
+        so that the two are in the same coordinate system.
+        this is needed to project properly
+    """
+    syms, xyzs = ar.geom.read(
+        output_string,
+        start_ptt=app.padded(app.NEWLINE).join([
+            app.escape('ATOMIC COORDINATES'),
+            app.LINE, app.LINE, app.LINE, '']),
+        line_start_ptt=app.UNSIGNED_INTEGER,
+        line_sep_ptt=app.FLOAT,)
+    geo = automol.geom.from_data(syms, xyzs, angstrom=False)
+    return geo
+
+
 def opt_zmatrix(output_string):
     """ get optimized z-matrix geometry from output
     """
