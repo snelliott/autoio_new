a5ab001ee41a6036ff218589195a0eae5da172f0
Kevin Moore <kmoore@blueslogin4.lcrc.anl.gov>
2020-05-06 14:17:23 -0500

    update for dist test

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 
 setup(name="elstruct",
-      version="0.2.0",
+      version="0.1.0",
       packages=["elstruct",
                 "elstruct.writer",
                 "elstruct.reader",
