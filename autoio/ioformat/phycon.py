6cd5b4a45cefd89b2ad7056fcde9ce59980d41f9
Kevin Moore <kmoore@blueslogin1.lcrc.anl.gov>
2020-12-23 14:47:57 -0600

    update ioformat docstrings

--- autoio/ioformat/phycon.py
+++ autoio/ioformat/phycon.py
@@ -1,17 +1,18 @@
-""" Physical constants and conversion factors
+""" Library of physical constants and conversion factors
 """
 
 from qcelemental import constants as qcc
 
-# physical constants
+
+# Physical Constants
 NAVO = 6.0221409e+23
 RC = 1.98720425864083  # gas constant in cal/(mol.K)
-RC_kcal = 1.98720425864083e-3  # gas constant in kcal/(mol.K)
+RC_KCAL = 1.98720425864083e-3  # gas constant in kcal/(mol.K)
 RC2 = 82.0573660809596  # gas constant in cm^3.atm/(mol.K)
-RC_cal = 1.98720425864083  # gas constant in cal/(mol.K)
-RC_atm = 82.0573660809596  # gas constant in cm^3.atm/(mol.K)
+RC_CAL = 1.98720425864083  # gas constant in cal/(mol.K)
+RC_ATM = 82.0573660809596  # gas constant in cm^3.atm/(mol.K)
 
-# conversion factors
+# Conversion Factors
 KCAL2CAL = qcc.conversion_factor('kcal/mol', 'cal/mol')
 J2CAL = qcc.conversion_factor('J/mol', 'cal/mol')
 KJ2CAL = qcc.conversion_factor('kJ/mol', 'cal/mol')
