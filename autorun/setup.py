42ae7cd2178449849187829d00002c808eb9cd13
snelliott <sarah.elliott26@uga.edu>
2021-04-21 12:51:33 -0500

    setup autorun for conda build

---
+++ autorun/setup.py
@@ -0,0 +1,11 @@
+""" Install autorun
+"""
+from distutils.core import setup
+
+
+setup(name="autorun",
+      version="0.1.0",
+      packages=["autorun",
+                "autorun.tests"])
+               # "autorun.aux"])#,
+               # "autorun.tests"])
