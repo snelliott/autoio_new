a63fc199afb24e2f0411530500f11bdb242dd20d
snelliott <sarah.elliott26@uga.edu>
2021-04-21 16:33:35 -0500

    ci12

--- autorun/setup.py
+++ autorun/setup.py
@@ -6,6 +6,9 @@ from distutils.core import setup
 setup(name="autorun",
       version="0.1.0",
       packages=["autorun",
-                "autorun.tests"])
+                "autorun.tests",
+                "autorun.tests.data"],
                # "autorun.aux"])#,
                # "autorun.tests"])
+      package_data={
+          'autorun': ['autorun/tests/data/*'])
