8dadcf73d21a53b3103bc53f61714b0ce53aeab5
Kevin Moore <kmoore@blueslogin3.lcrc.anl.gov>
2021-05-21 13:37:47 -0500

    update mess well lump readers

--- autoio-interfaces/mess_io/reader/_lump.py
+++ autoio-interfaces/mess_io/reader/_lump.py
@@ -19,9 +19,24 @@ def merged_wells(mess_aux_str):
         if 'number of species =' in line:
             merge_well_lines.append(mess_lines[i+1])
 
-    # Parse the merged wells
+    # Read the block of text that has all of the species
+    start_idx = None
+    for i, line in enumerate(mess_lines):
+        if 'number of species =' in line:
+            start_idx = i+1
+            num_merged_spc = int(line.strip().split()[-1])
+            break
+
+    # Use the number of merged species and line idx to get lines of all wells
     merged_well_lst = ()
-    for line in merge_well_lines:
-        merged_well_lst += (tuple(line.strip().split()),)
+    if start_idx is not None:
+        merge_well_lines = []
+        for idx in range(num_merged_spc):
+            merge_well_lines.append(mess_lines[start_idx+2*idx])
+
+        # Parse the merged wells
+        merged_well_lst = ()
+        for line in merge_well_lines:
+            merged_well_lst += (tuple(line.strip().split()),)
 
     return merged_well_lst
